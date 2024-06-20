8c01e6474652258fd9b05e8e60f5b58f488c817e
Gaurav <sahityagaurav.41125@gmail.com>
2024-06-20 17:35:50 +0530

    feat: added clipboard docs

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-let message = document.getElementById("message");
-const name = document.currentScript.getAttribute('data-payload');
+class Node {
+    constructor(name, email, age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.link = null;
+    }
+}
 
-console.log('name', JSON.parse(name));
+class LinkedList {
+    constructor() {
+        this.list = null;
+        this.size = 0;
+        this.currentPointer = null;
+    }
 
-window.addEventListener('keydown', function (event) {
-    if (event.ctrlKey)
-        if (event.altKey)
-            if (event.key === 'l') {
-                message.innerHTML = 'Logout successfull';
-                message.classList.remove('text-orange-500');
-                message.classList.add('text-emerald-300');
+    insert(node) {
+        if (!this.list)
+            this.list = node;
+        else {
+            let current = this.list;
+
+            while (current.link) {
+                current = current.link;
             }
-});
+            current.link = node;
+        }
+        ++this.size;
+    }
+
+    insertAtBegining(node) {
+        node.link = this.list;
+        this.list = node;
+        ++this.size;
+    }
+
+    retrieve() {
+        this.currentPointer = this.list;
+        return this.currentPointer;
+    }
+}
+
+const linkedList = new LinkedList();
+
+linkedList.insert(new Node("gaurav", "gaurav@gmail.com", 24));
+linkedList.insert(new Node("ankit", "ankit@gmail.com", 26));
+linkedList.insert(new Node("perry", "perry@gmail.com", 22));
+linkedList.insertAtBegining(new Node("gurmeet", "gurmeet@gmail.com", 24));
+
+console.log(linkedList.list);
+console.log(linkedList.size);
